47a262540cd6769bdacb7df1217a6d1fcfce2aee
TokyoProgramming <samuraidjango.jp@gmail.com>
2021-01-04 02:37:29 +0900

    add color

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const selectElement = document.querySelector('.colors');
 
 // variables
 let painting = false;
+let color = 'yellow';
 let x = 0;
 let y = 0;
 
@@ -31,15 +33,21 @@ const draw = (e) => {
 
 const drawLine = (ctx, x1, y1, x2, y2) => {
   ctx.beginPath();
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 1;
+  ctx.strokeStyle = `${color}`;
+  ctx.lineWidth = 10;
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
   ctx.closePath();
 };
 
+const changeColor = (e) => {
+  color = e.target.value;
+};
+
 // EventListeners
 canvas.addEventListener('mousedown', startPosition);
 window.addEventListener('mouseup', endPosition);
 canvas.addEventListener('mousemove', draw);
+
+selectElement.addEventListener('change', changeColor);
