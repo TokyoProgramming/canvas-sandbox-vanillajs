81341f874a73e871ff62334e7a47da75f8876ffb
TokyoProgramming <samuraidjango.jp@gmail.com>
2021-01-05 00:10:20 +0900

    add shape function

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ const canvasArea = document.getElementById('canvas-area');
 const hamburgerIcon = document.getElementById('hamburger');
 const menu = document.getElementById('menubar');
 const deleteIcon = document.getElementById('delete');
+const selectShape = document.querySelector('.shape');
 
 // variables
 let painting = false;
 let color = 'black';
 let lineWidth = 1;
+let shape = 'line';
 let x = 0;
 let y = 0;
 textarea = null;
+
 const startPosition = (e) => {
-  x = e.offsetX;
-  y = e.offsetY;
-  painting = true;
+  if (shape === 'line') {
+    x = e.offsetX;
+    y = e.offsetY;
+    painting = true;
+  }
 };
 
 const endPosition = (e) => {
@@ -46,9 +51,9 @@ const drawLine = (ctx, x1, y1, x2, y2) => {
   ctx.stroke();
   ctx.closePath();
 };
+
 // Change Color
 const changeColor = (e) => {
-  color = e.target.value;
   canvas.classList.remove('canvas-pen');
 
   // Change cursor
@@ -58,6 +63,7 @@ const changeColor = (e) => {
     console.log('eraser');
   }
 };
+
 // Change Line Width
 const changeLineWidth = (e) => {
   lineWidth = e.target.value;
@@ -71,12 +77,39 @@ const clearCanvas = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
+const drawCircle = (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  if (shape === 'arc') {
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  } else if (shape === 'rectangle') {
+    ctx.beginPath();
+    ctx.rect(x, y, 150, 100);
+    ctx.stroke();
+  } else if (shape === 'square') {
+    ctx.beginPath();
+    ctx.rect(x, y, 100, 100);
+    ctx.stroke();
+  }
+};
+
+// Change Shape
+const changeSelectShape = (e) => {
+  shape = e.target.value;
+  console.log(shape);
+};
+
 // EventListeners
 canvas.addEventListener('mousedown', startPosition);
 window.addEventListener('mouseup', endPosition);
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('dblclick', drawCircle);
+
 hamburgerIcon.addEventListener('click', toggleMenu);
 deleteIcon.addEventListener('click', clearCanvas);
 
 selectElement.addEventListener('change', changeColor);
 selectLine.addEventListener('change', changeLineWidth);
+selectShape.addEventListener('change', changeSelectShape);
